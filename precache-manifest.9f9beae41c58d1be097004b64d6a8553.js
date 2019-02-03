570ffb6ba7d157aa7048cae32b587f5e2b98ded6
Khadidja <Khadidja@users.noreply.github.com>
2019-02-02 22:34:39 -0500

    Updates

---
+++ precache-manifest.9f9beae41c58d1be097004b64d6a8553.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "36204b362514e866e4e3",
+    "url": "/react-todo/static/js/main.36204b36.chunk.js"
+  },
+  {
+    "revision": "05406675fae3b635d781",
+    "url": "/react-todo/static/js/1.05406675.chunk.js"
+  },
+  {
+    "revision": "36204b362514e866e4e3",
+    "url": "/react-todo/static/css/main.29409df0.chunk.css"
+  },
+  {
+    "revision": "1082c92e27b91597cbcfb83a21bb8d45",
+    "url": "/react-todo/index.html"
+  }
+];
