71f8b7fe7a71c2518411bca9c9033e41e9011958
Khadidja <Khadidja@users.noreply.github.com>
2019-02-02 21:51:42 -0500

    added about component page using react router

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header () {
   return (
     <header className='jumbotron text-center'>
       <h1>To-Do List</h1>
+      <Link style={linkStyle} to='/'>
+        Home
+      </Link>
+      <Link style={linkStyle} to='/about'>
+        About
+      </Link>
     </header>
   )
+}
+
+const linkStyle = {
+  margin: '16px',
+  borderBottom: '1px solid'
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About () {
+  return (
+    <React.Fragment>
+      <h1 className='page-header'>About</h1>
+      <p>A To-Do list app to learn React.</p>
+    </React.Fragment>
+  )
+}
