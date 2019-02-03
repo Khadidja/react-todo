be87e2dd02666c884190b58dcf59ccad9be10a93
Khadidja <Khadidja@users.noreply.github.com>
2019-02-03 01:55:31 -0500

    Updates

--- precache-manifest.832aad35a50c16233fec0ae60d7f7c9a.js
+++ precache-manifest.832aad35a50c16233fec0ae60d7f7c9a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "https://khadidja.github.io/react-todo/static/js/runtime~main.a886cf60.js"
   },
   {
-    "revision": "035f9e49f67959d0c130",
-    "url": "https://khadidja.github.io/react-todo/static/js/main.035f9e49.chunk.js"
+    "revision": "29687fe3e8017b08803b",
+    "url": "https://khadidja.github.io/react-todo/static/js/main.29687fe3.chunk.js"
   },
   {
     "revision": "05406675fae3b635d781",
     "url": "https://khadidja.github.io/react-todo/static/js/1.05406675.chunk.js"
   },
   {
-    "revision": "035f9e49f67959d0c130",
+    "revision": "29687fe3e8017b08803b",
     "url": "https://khadidja.github.io/react-todo/static/css/main.29409df0.chunk.css"
   },
   {
-    "revision": "54039ce449306a6a1652a1ea96abe296",
+    "revision": "9282996519f8c407fece492f23132445",
     "url": "https://khadidja.github.io/react-todo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://khadidja.github.io/react-todo/precache-manifest.a652f4a18ca8e6d6e311ad4f560fa7b4.js"
+  "https://khadidja.github.io/react-todo/precache-manifest.832aad35a50c16233fec0ae60d7f7c9a.js"
 );
 
 workbox.clientsClaim();
